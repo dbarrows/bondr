62827e8edb340212d8d05b70f0feefb706ad8073
Dexter Barrows <dexter@barrows.io>
2021-06-24 17:31:01 -0400

    uint -> unsigned int for windows

--- src/stub.cpp
+++ src/stub.cpp
@@ -6,7 +6,7 @@ arma::vec propensities_cpp(SEXP rnet_xptr, arma::vec v) {
     auto net = *Rcpp::XPtr<bondr::rnet>(rnet_xptr);
     
     arma::vec p = arma::vec(net.reactions.size());
-    for (uint i = 0; i < p.size(); i++)
+    for (unsigned int i = 0; i < p.size(); i++)
         p[i] = net.reactions[i].propensity(v);
 
     return p;
